0f20bdefccd33a376f0446abec84ef0b9d405371
Maki44 <sebhatumakda20@gmail.com>
2021-03-22 20:51:26 +0100

    added json file for node state

--- test/simulator_test.js
+++ test/simulator_test.js
@@ -1,15 +1,15 @@
-const { koi_node }       = require('../index.js')
+const { koi_node } = require("../index.js");
 
 // var wallet1 = "/Users/abelsebhatu/Desktop/koi-protocol/dist/keywallet.json";
-var wallet1 = "/media/al/files/koi/Arweave/sec2.json"
+var wallet1 = "/Users/makdasebhatu/Documents/my-wallet/Arweave/keywallet.json";
 
 const arg1 = {
-        wallet:wallet1,
-        qty:200
-      }
+  wallet: wallet1,
+  qty: 200,
+};
 
-var node = new koi_node (arg1);
+var node = new koi_node(arg1);
 
-console.log('node is', node)
+console.log("node is", node);
 
-node.run()
+node.run();
