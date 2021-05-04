95b2e16bfd5fd142fd5de9b47f3abc180b939afa
Andre Vallestero <andrevallestero@gmail.com>
2021-05-04 14:57:30 -0400

    improved test whitespace

--- test/mnemonic_test.js
+++ test/mnemonic_test.js
@@ -4,21 +4,21 @@ const ktools_load = new koi_tools();
 
 async function test() {
     // Test wallet generation
-    console.log("Generating wallet using mnemonic, this can take a minute")
+    console.log("\nGenerating wallet using mnemonic, this can take a minute")
     if (await ktools.generateWallet(true))
-        console.log(`Wallet generated\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}`);
+        console.log(`Wallet generated\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}\n`);
     else return "Failed to generate wallet";
 
     // Save mnemonic
     let mnemonic = ktools.mnemonic;
 
     // Create new ktools instance
-    console.log(`Wallet should be empty\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}`);
+    console.log(`New wallet should be empty\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}\n`);
 
     // Load wallet
     console.log("Loading wallet using mnemonic, this can a take minute")
     if (await ktools_load.loadWallet(mnemonic))
-        console.log(`Wallet loaded using mnemonic\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}`);
+        console.log(`Wallet loaded using mnemonic\nmnemonic: ${ktools.mnemonic}\naddress: ${ktools_load.address}\n`);
     else return "Failed to load wallet";
     
     return "Success";
