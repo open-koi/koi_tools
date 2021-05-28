3e7c22fa4329e664542e7841db361716fa3f8f63
Andre Vallestero <andrevallestero@gmail.com>
2021-05-28 15:07:06 -0400

    Removed getTopContent

--- src/web.ts
+++ src/web.ts
@@ -1,48 +1,26 @@
-import { Common, getCacheData, ADDR_BUNDLER_TOP } from "./common";
+import { Common } from "./common";
 
 export class Web extends Common {
   /**
    * Get top contents of user
    * @returns Array of user contents
    */
-  async myContent(): Promise<[any]> {
-    // Getting top content is faster than entire state
-    const topContent: any = await getCacheData(ADDR_BUNDLER_TOP);
-    const contents: any = [];
+  async myContent(): Promise<Array<any>> {
+    // Get nft records
+    const state: any = await this.getContractState();
+    const registerRecords = state.registeredRecord;
 
-    topContent.data
-      .filter((item: any) => item.owner === this.address)
-      .forEach((element: any, i: number) => {
-        // let str_created_at = element.createdAt || "1609500000";
-        // let created_at = Number(str_created_at) * 1000;
-        // element.created_at = created_at;
-        element.order = i + 1;
-        contents.push(element);
-      });
+    // Get array of my awaitable NFTs
+    const contentViewProms = registerRecords.reduce(
+      (accu: any[], txId: string) => {
+        if (registerRecords[txId] === this.address)
+          accu.push(this.contentView(txId, state));
+      },
+      []
+    );
 
-    /*
-    const registerRecords = state.data.registeredRecord;
-    for (const txId in registerRecords) {
-      if (registerRecords[txId] == this.address) {
-        const nftInfo: any = await this.contentView(txId, state);
-        if (nftInfo !== null) {
-          contents.push(nftInfo);
-        }
-      }
-    }
-    */
-
-    return contents;
-  }
-
-  /**
-   *
-   * @returns
-   */
-  async getTopContent() {
-    const path = ADDR_BUNDLER_TOP;
-    const topContents = await getCacheData(path);
-    return topContents;
+    // Process NFTs simultaneously then return
+    return await Promise.all(contentViewProms);
   }
 }
 
