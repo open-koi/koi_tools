a97ac30d4492bcc1c080149333b2b30849786542
Andre Vallestero <39736205+AndreVallestero@users.noreply.github.com>
2021-05-31 11:46:39 -0400

    Removed redis load on init (#34)

--- test/node.test.js
+++ test/node.test.js
@@ -1,19 +1,12 @@
-/**
- * Testing node requires redis and .env setup
- */
-
 let knode = require("../dist/node");
+let ktools = new knode.Node();
 
 test("Test loading", () => {
   expect(true).toBe(true);
 })
 
-/*
-// These tests must be disabled when publishing as CI doesn't have Redis
-let ktools = new knode.Node();
 test("Test validate data null", async () => {
   jest.setTimeout(20000)
   const res = await ktools.validateData(39);
   expect(res).toBe(null);
 })
-*/
